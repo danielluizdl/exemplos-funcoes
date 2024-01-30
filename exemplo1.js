306a74d4fcdacd736c2d7e31859e79389df463ee
Daniel Luiz <loureirodlg@gmail.com>
2024-01-30 20:26:46 -0300

    jeito certo

--- exemplo1.js
+++ exemplo1.js
@@ -1,5 +1,19 @@
-function imprimirNomes(nome, idade, altura, profissao) {
-    console.log(`Olá! Meu nome é ${nome}, sou um jovem de ${idade} anos, ${altura} de altura e sou ${profissao}.`)
-}
+// function imprimirNomes(nome, idade, altura, profissao) {
+//     console.log(`Olá! Meu nome é ${nome}, sou um jovem de ${idade} anos, ${altura} de altura e sou ${profissao}.`)
+// }
 
-imprimirNomes("Joao", "12", "1.4", "estudante")
+// imprimirNomes("Joao", "12", "1.4", "estudante")
+
+// Correcao do exercicio
+
+const pessoaObj = {
+    nome: "Joao",
+    idade: 12,
+    altura: 1.4,
+    profissao: "estudante"
+};
+
+function apresentar(pessoa) {
+    console.log(`Olá! Meu nome é ${pessoa.nome}, sou um jovem de ${pessoa.idade} anos, ${pessoa.altura} de altura e sou ${pessoa.profissao}.`)
+}
+apresentar(pessoaObj)
