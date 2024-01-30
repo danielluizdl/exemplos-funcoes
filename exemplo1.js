87643f648aba3e8be4df50b2090769c3808b235e
Daniel Luiz <loureirodlg@gmail.com>
2024-01-30 20:22:12 -0300

    first commit

---
+++ exemplo1.js
@@ -0,0 +1,5 @@
+function imprimirNomes(nome, idade, altura, profissao) {
+    console.log(`Olá! Meu nome é ${nome}, sou um jovem de ${idade} anos, ${altura} de altura e sou ${profissao}.`)
+}
+
+imprimirNomes("Joao", "12", "1.4", "estudante")
